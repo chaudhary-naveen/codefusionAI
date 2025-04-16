f261308b28cd070c328938a6fe7fe98544b02641
Naveen Chaudhary <98143534+chaudhary-naveen@users.noreply.github.com>
2025-04-16 23:10:52 +0530

    Build

--- client/src/path.js
+++ client/src/path.js
@@ -1,9 +1,9 @@
-const path = "http://localhost:5001/api/";
+// const path = "http://localhost:5001/api/";
 // const path = "https://venomcodebackend.onrender.com/";
 // const path = "http://192.168.43.16:5000/";
 // const path = "https://venomcodebackend.cyclic.app/";
 // const path = "https://code-fusion-nu.vercel.app/";
-// const path = '/api/';
+const path = '/api/';
 const version = "AI ";
 const youtubeLogoLink = 'https://youtu.be/6v0UNrEnmPA?si=jbHqTY65NCQVYbTP';
 export default path;
